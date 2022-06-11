6b5a53d320bf9b02e369908dfee9f911b1e24ffd
jstafford <jwstafford1991@gmail.com>
2022-06-10 20:43:24 -0500

    Connected graphql with resolvers to express

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import express from "express";
 import { expressjwt } from "express-jwt";
 import jwt from "jsonwebtoken";
+import { readFile } from "fs/promises";
+
 import { User } from "./db.js";
+import { resolvers } from "./resolvers.js";
 
 const PORT = 9000;
 const JWT_SECRET = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
@@ -30,8 +33,10 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const typeDefs = readFile();
-const apolloServer = new ApolloServer({ typeDefs });
+const typeDefs = await readFile("./schema.graphql", "utf8");
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+apolloServer.applyMiddleware({ app, path: "/graphql" });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
