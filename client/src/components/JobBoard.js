eb01e34a3a8cc93de712bd41cf98027370741f79
jstafford <jwstafford1991@gmail.com>
2022-06-10 20:38:05 -0500

    Reformatted the code using prettier, so I no longer receive extra changes with my commits.
    Also, started adding graphql to server.js and created schema.graphql

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,12 +1,10 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from "./JobList";
+import { jobs } from "../fake-data";
 
 function JobBoard() {
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
--- client/src/fake-data.js
+++ client/src/fake-data.js
@@ -1,28 +1,29 @@
-const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const LOREM_IPSUM =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 export const companies = [
   {
-    id: 'company1',
-    name: 'Company A',
+    id: "company1",
+    name: "Company A",
     description: LOREM_IPSUM,
   },
   {
-    id: 'company2',
-    name: 'Company B',
+    id: "company2",
+    name: "Company B",
     description: LOREM_IPSUM,
   },
 ];
 
 export const jobs = [
   {
-    id: 'job1',
-    title: 'Job 1',
+    id: "job1",
+    title: "Job 1",
     company: companies[0],
     description: LOREM_IPSUM,
   },
   {
-    id: 'job2',
-    title: 'Job 2',
+    id: "job2",
+    title: "Job 2",
     company: companies[1],
     description: LOREM_IPSUM,
   },
