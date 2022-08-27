2730b4f3e0932582a0f31f4bef36cdd933d29a1b
jstafford <jwstafford1991@gmail.com>
2022-08-27 11:14:18 -0500

    Setting username and password so I don't have to type it in.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { login } from "../auth";
 
 function LoginForm({ onLogin }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("alice@facegle.io");
+  const [password, setPassword] = useState("alice123");
   const [error, setError] = useState(false);
 
   const handleSubmit = async (event) => {
